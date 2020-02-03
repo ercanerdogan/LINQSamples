87e6b79da21149030b75a206a2a2f1f05a67ff17
Ercan Erdoğan <ercanerdogan@gmail.com>
2020-02-03 17:16:12 +0300

    -comparing between 2 array items

--- GroupByLINQ/Program.cs
+++ GroupByLINQ/Program.cs
@@ -11,6 +11,23 @@ namespace GroupByLINQ
     {
         static void Main(string[] args)
         {
+
+            //int[] numbersA = { 0, 2, 4, 5, 6, 8, 9 };
+            //int[] numbersB = { 1, 3, 5, 7, 8 };
+
+            //var pairs = from a in numbersA
+            //            from b in numbersB
+            //            where a < b
+            //            select (a, b);
+
+            //Console.WriteLine("Pairs where a<b : ");
+            //foreach (var pair in pairs)
+            //{
+            //    Console.WriteLine($"{pair.a} is less than {pair.b}");
+
+            //}
+
+
             string jsonData = "[" +
                     "{\"Title\":\"Sphere\", \"Author\":\"Michael Crichton\", \"Genre\":\"ScienceFiction\", \"IsFiction\": true}," +
                     "{\"Title\":\"Jurassic Park\",\"Author\":\"Michael Crichton\", \"Genre\":\"ScienceFiction\",\"IsFiction\": true},"+
